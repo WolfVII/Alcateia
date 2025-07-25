5632e1b1bb409f702743b99e31210a841807ee8c
WolfVII <sergantonio91@gmail.com>
2025-07-25 19:57:10 -0300

    engine.cpp

---
+++ core/engine.cpp
@@ -0,0 +1,17 @@
+#include "engine.h"
+#include "../renderer/renderer.h"
+
+Engine::Engine() {}
+Engine::~Engine() {}
+
+bool Engine::Initialize(HINSTANCE hInstance, int nCmdShow) {
+    return Renderer::InitializeWindow(hInstance, nCmdShow);
+}
+
+void Engine::Run() {
+    Renderer::MainLoop();
+}
+
+void Engine::Shutdown() {
+    Renderer::Cleanup();
+}
