d26179058ef95b76f3ec416ecb8fa2f4c59ad552
WolfVII <sergantonio91@gmail.com>
2025-07-26 18:43:08 -0300

    Update engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,17 +1,40 @@
+// ======================= engine.cpp =======================
 #include "engine.h"
 #include "renderer.h"
 
-Engine::Engine() {}
-Engine::~Engine() {}
+static Engine::Mode currentMode = Engine::OBJECT_MODE;
 
-bool Engine::Initialize(HINSTANCE hInstance, int nCmdShow) {
-    return Renderer::InitializeWindow(hInstance, nCmdShow);
+void Engine::Init(HWND hwnd) {
+    Renderer::Init(hwnd);
 }
 
-void Engine::Run() {
-    Renderer::MainLoop();
+void Engine::Update() {
+    // Lógica futura
+}
+
+void Engine::Render() {
+    Renderer::Begin();
+    if (currentMode == OBJECT_MODE)
+        Renderer::RenderObjectMode();
+    else
+        Renderer::RenderGameMode();
+    Renderer::End();
 }
 
 void Engine::Shutdown() {
-    Renderer::Cleanup();
+    Renderer::Shutdown();
+}
+
+LRESULT CALLBACK Engine::HandleMessage(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_KEYDOWN:
+        if (wParam == VK_RETURN) currentMode = GAME_MODE;
+        if (wParam == VK_ESCAPE) currentMode = OBJECT_MODE;
+        return 0;
+    default:
+        return Renderer::HandleInput(hWnd, msg, wParam, lParam);
+    }
 }
