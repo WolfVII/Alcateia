ebda1c7a9aef61f3a09bc5e6be24f62a41804583
WolfVII <sergantonio91@gmail.com>
2025-07-26 18:46:27 -0300

    Update renderer.h

--- src/renderer.h
+++ src/renderer.h
@@ -1,8 +1,17 @@
-#pragma once
+// ======================= renderer.h =======================
+#ifndef RENDERER_H
+#define RENDERER_H
+
 #include <windows.h>
 
 namespace Renderer {
-    bool InitializeWindow(HINSTANCE hInstance, int nCmdShow);
-    void MainLoop();
-    void Cleanup();
+    void Init(HWND hwnd);
+    void Shutdown();
+    void Begin();
+    void End();
+    void RenderObjectMode();
+    void RenderGameMode();
+    LRESULT CALLBACK HandleInput(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 }
+
+#endif
